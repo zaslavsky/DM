31be08fe54750d32c40b9788e76457149a358e18
zaslavsky <zaslavsky@live.ru>
2014-04-20 20:41:19 +0400

    Data
    Data save into .xls file

--- ad.py
+++ ad.py
@@ -1,27 +1,27 @@
 #!/usr/bin/env python
 #-*- encoding: cp1251 -*-
 
-import json, urllib, xlwr, xlrd
+import json, urllib, xlrd, xlwt
 
+
+file_data = xlwt.Workbook().add_sheet('Main')
 
 url_main = "http://api.worldoftanks.ru/wot/encyclopedia/tankinfo/?application_id=09be322fee8ba81d502df3cd5a03a6c5"
 
-
-
-
-
-
-
-
-txt_data = urllib.urlopen(url_main+"&tank_id=81").read()
+txt_data = urllib.urlopen(url_main+"&tank_id=1").read()
 
 data = json.loads(txt_data)
 
-print data["data"]["81"]["weight"]
+print data["data"]["1"]["weight"]
 
-def main():
+
+def data_save():
 	
-	return 0
+	file_data.write(0, 0, 'Player')
+	file_data.write(1, 0, 7)
+	#ws.write(2, 2, xlwt.Formula("A3+B3"))
+	
+	file_data.save('data.xls')
 
-
-
+data_save()
+	
